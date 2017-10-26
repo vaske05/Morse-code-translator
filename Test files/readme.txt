fd82e24409b2cb7b4b78e63bc3c2aa79b2bc80fd
Milan Vasic <vaske494@gmail.com>
2017-10-26 03:39:51 +0200

    Final commit.

---
+++ Test files/readme.txt
@@ -0,0 +1,8 @@
+This folder contains public data set for 'Morse code' homework task.
+
+Content:
+*.in - text files that contain input signals
+*.out - text files that contain expected output
+
+Example:
+For test case 6, you can find input signal in case-6.in file and expected output in case-6.out file.
---
+++ main.cpp
@@ -0,0 +1,287 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <windows.h>
+#include <sstream>
+#include <chrono>
+#include <stdlib.h>
+using namespace std;
+
+
+bool numberOfSamples(string path)
+{
+	ifstream file(path);
+	string line = "";
+	int lineCounter = 0;
+
+	if (!file.is_open()) {
+		exit(EXIT_FAILURE);
+	}
+
+	while (getline(file, line)) {
+		lineCounter++;
+	}
+
+	cout << endl << "Number of samples in file is: " << lineCounter << "\n";
+	if (lineCounter > 10000)
+	{
+		cout << "Error! Maximal number of samples per signal is: 10.000 \n";
+		return false;
+	}
+	else
+	{
+		cout << "Correct number of samples. \n \n";
+		return true;
+	}
+}
+
+int lenOfFirstChar(string path)
+{
+	ifstream file(path);
+	string line = "";
+	double number = 0;
+	int counter = 0;
+
+	while (getline(file, line))
+	{
+		number = atof(line.c_str());
+
+		if (number >= 0.510 && number <= 1.510)
+		{
+			counter++;
+		}
+		else
+		{
+			return counter;
+		}
+	}
+}
+
+//Morse to English
+string convertToEnglish(std::string morse, std::string const morseCode[])
+{
+	std::string output = "";
+	std::string currentLetter = "";
+	std::istringstream ss(morse);
+
+	std::size_t const characters = 27;
+
+	while (ss >> currentLetter)
+	{
+		if (currentLetter == "/")
+		{
+			output += " ";
+			ss >> currentLetter;
+		}
+
+		std::size_t index = 0;
+		try
+		{
+			while (currentLetter != morseCode[index] && index < characters)
+			{
+
+				++index;
+			}
+		}
+		catch (const std::exception& e)
+		{
+			cout << "Exception: " << e.what() << endl;
+		}
+		
+
+		output += 'a' + index;
+	}
+
+	return output;
+}
+
+
+int main()
+{
+	string path;
+	cout << "Enter path of file.(Example: C:/Users/Vaske/Desktop/Test/case-7.in ) " << endl;
+	cin >> path;
+	//string path = "C:/Users/Vaske/Desktop/Test/case-0.in";
+
+	ifstream file(path);
+	string line = "";
+	string morseCode[250];
+	string morseCodeString;
+	int lenOfDot, lenOfDash, lenOfWordsPause, lenOfLetterPause, lenOfSmallPause, i = 0;
+
+	std::string const morse[] = { ".-", "-...", "-.-.", "-..", ".", "..-.",
+		"--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-",
+		".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."," / " };
+
+	if (!file.is_open()) {
+		cout << "Error! File not found." << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	if (numberOfSamples(path)) //Ako je zadovoljavajuci velicine fajla(max 10 000 redova)
+	{
+
+		int temp = lenOfFirstChar(path); //Duzina prvog znaka
+		if (temp < 22 && temp > 5)
+		{
+			lenOfDot = temp;
+			//cout << "Length of dot(.): " << lenOfDot <<"\n"; // .
+
+			lenOfDash = 3 * lenOfDot;
+			//cout << "Length of dash(-): " << lenOfDash << "\n"; // -
+
+			lenOfWordsPause = 7 * lenOfDot;
+			//cout << "Length of words pause( / ): " << lenOfWordsPause << "\n"; // pause between words( / ) 3xSpace
+
+			lenOfLetterPause = lenOfDash;
+			//cout << "Length of letter pause( ): " << lenOfLetterPause << "\n"; // pause between letter( )
+
+			lenOfSmallPause = lenOfDot;
+			//cout << "Length of small pause( ): " << lenOfSmallPause << "\n"; // small pause()
+
+		}
+		else if (temp % 3 == 0)
+		{
+			lenOfDash = temp;
+			//cout << "Length of dash(-):" << lenOfDash<<"\n"; // -
+
+			lenOfDot = lenOfDash / 3;
+			//cout << "Length of dot(.): " << lenOfDot << "\n"; // .
+
+			lenOfWordsPause = 7 * lenOfDot;
+			//cout << "Length of words pause( / ): " << lenOfWordsPause << "\n"; // pause between words( / ) 3xSpace
+
+			lenOfLetterPause = lenOfDash;
+			//cout << "Length of letter pause( ): " << lenOfLetterPause << "\n"; // pause between letter( )
+
+			lenOfSmallPause = lenOfDot;
+			//cout << "Length of small pause( ): " << lenOfSmallPause << "\n"; // small pause()
+		}
+
+		//Signal to Morse------------------------------------------------------------
+		int counter = 0;
+		while (getline(file, line))
+		{
+			++counter;
+			double number = atof(line.c_str());
+			//cout << number << "\n";
+
+			if (number >= 0.520 && number <= 1.600)
+			{
+				if (counter == lenOfDot || counter == lenOfDash)
+				{
+					getline(file, line);
+					number = atof(line.c_str());
+					++counter;
+					if (number >= 0.520 && number <= 1.600 || counter - 1 == lenOfDash)
+					{
+						if (counter - 1 == lenOfDash)
+						{
+							morseCode[i] = "-";
+							morseCodeString += "-";
+							++i;
+							counter = 1;
+						}
+					}
+					else
+					{
+						morseCode[i] = ".";
+						morseCodeString += ".";
+						++i;
+						counter = 1;
+					}
+				}
+			}
+
+			else if (number >= -0.600 && number <= 0.580)
+			{
+				if (counter == lenOfLetterPause || counter == lenOfWordsPause || counter == lenOfSmallPause)
+				{
+					getline(file, line);
+					number = atof(line.c_str());
+					++counter;
+					if (number >= -0.600 && number <= 0.580 || counter - 1 == lenOfWordsPause)
+					{
+						if (counter - 1 == lenOfWordsPause)
+						{
+							morseCode[i] = " / ";
+							morseCodeString += " / ";
+							++i;
+							counter = 1;
+						}
+					}
+					else if (counter - 1 == lenOfLetterPause)
+					{
+						morseCode[i] = " ";
+						morseCodeString += " ";
+						++i;
+						counter = 1;
+					}
+					else if (counter - 1 == lenOfSmallPause)
+					{
+						morseCode[i] = "";
+						morseCodeString += "";
+						++i;
+						counter = 1;
+					}
+				}
+			}
+		}
+		//---------------------------------------------------------------------------
+	}
+	else {
+		exit(EXIT_FAILURE);
+	}
+
+	cout << endl << "Input Morse code: " << morseCodeString << endl;
+
+	cout << endl << "Input text: " << convertToEnglish(morseCodeString, morse) << endl << endl;
+
+
+	//Simulate signal
+	string answer;
+	cout << "Simulate magic?" << endl;
+
+	while (answer != "Y" || answer != "N")
+	{
+		cout << "Y or N" << endl;
+		cin >> answer;
+
+		if (answer == "Y")
+		{
+			for (int x = 0; x < i; x++)
+			{
+				if (morseCode[x] == ".")
+				{
+					Beep(500, 200);
+				}
+				else if (morseCode[x] == "-")
+				{
+					Beep(500, 700);
+				}
+				else if (morseCode[x] == " ")
+				{
+					Beep(0, 100);
+				}
+				else if (morseCode[x] == " / ")
+				{
+					Beep(0, 400);
+				}
+			}
+			cout << endl;
+			cout << "---------------------------------------------------" ;
+			cout << endl << "Developed by Milan Vasic." << endl;
+			cout << "---------------------------------------------------" << endl;
+			exit(EXIT_SUCCESS);
+		}
+		else if (answer == "N") {
+			cout << endl;
+			cout << "---------------------------------------------------";
+			cout << endl << "Developed by Milan Vasic." << endl;
+			cout << "---------------------------------------------------" << endl;
+			exit(EXIT_SUCCESS);
+		}
+	}
+}
