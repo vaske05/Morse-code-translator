594b8b16b3c519bde3028c1f57e9814b58c06c01
Milan Vasic <vaske494@gmail.com>
2019-08-22 19:07:33 +0200

    Refactoring

--- Converter.h
+++ Converter.h
@@ -6,7 +6,10 @@ class Converter
 {
 private: 
 	//Signal parameters
-	int lenOfDot, lenOfDash, lenOfWordsPause, lenOfLetterPause, lenOfSmallPause, i = 0;
+	int lenOfDot, lenOfDash, lenOfWordsPause, lenOfLetterPause, lenOfSmallPause, arrayLength = 0;
+
+	string morseCodeArray[250], morseCodeString;
+
 
 	//Array of morse code signs(one sign = one letter)
 	string const morse[40] = { ".-", "-...", "-.-.", "-..", ".", "..-.",
@@ -15,7 +18,15 @@ private:
 
 public:
 	Converter();
-	string morseToEnglish(string morseCodeString);
+
+	string morseToEnglish();
+	void signalToMorse(string path);
+	void setSignalParameters(string path);
+	int lengthOfFirstChar(string path);
+	
+	string getMorseCodeString();
+	string* getMorseCodeArray();
+	int getArrayLength();
 
 };
 
--- main.cpp
+++ main.cpp
@@ -38,38 +38,19 @@ bool checkSamplesNumber(string path)
 	}
 }
 
-int lenOfFirstChar(string path)
+
+void print(Converter converter)
 {
-	ifstream file(path);
-	string line = "";
-	double number = 0;
-	int counter = 0;
+	cout << endl << "Input Morse code: " << converter.getMorseCodeString() << endl;
+	cout << endl << "Input text: " << converter.morseToEnglish() << endl << endl;
 
-	while (getline(file, line))
-	{
-		number = atof(line.c_str());
-
-		if (number >= 0.510 && number <= 1.510)
-		{
-			counter++;
-		}
-		else
-		{
-			return counter;
-		}
-	}
-}
-
-
-void print(string morseCodeString, string morseCode[], int i)
-{
-	Converter converter;
-	cout << endl << "Input Morse code: " << morseCodeString << endl;
-	cout << endl << "Input text: " << converter.morseToEnglish(morseCodeString) << endl << endl;
-
-
+	
 	//Simulate audio morse signal :)
-	string answer;
+	string answer, *morseCodeArray;
+	int arrayLength = converter.getArrayLength();
+
+	morseCodeArray = converter.getMorseCodeArray();
+
 	cout << "Simulate magic?" << endl;
 
 	while (answer != "Y" || answer != "N")
@@ -79,21 +60,21 @@ void print(string morseCodeString, string morseCode[], int i)
 
 		if (answer == "Y")
 		{
-			for (int x = 0; x < i; x++)
+			for (int i = 0; i < arrayLength; i++)
 			{
-				if (morseCode[x] == ".")
+				if (*(morseCodeArray + i) == ".")
 				{
 					Beep(500, 200);
 				}
-				else if (morseCode[x] == "-")
+				else if (morseCodeArray[i] == "-")
 				{
 					Beep(500, 700);
 				}
-				else if (morseCode[x] == " ")
+				else if (morseCodeArray[i] == " ")
 				{
 					Beep(0, 100);
 				}
-				else if (morseCode[x] == " / ")
+				else if (morseCodeArray[i] == " / ")
 				{
 					Beep(0, 400);
 				}
@@ -112,20 +93,18 @@ void print(string morseCodeString, string morseCode[], int i)
 			exit(EXIT_SUCCESS);
 		}
 	}
+	
 }
-
 
 int main()
 {
+	Converter converter;
 	string path;
 	//cout << "Enter path of file.(Example: C:/Users/Vaske/Desktop/Test/case-7.in ) " << endl;
 	//cin >> path;
 	path = "C:/tests/case-10.in";
 
 	ifstream file(path);
-	string line = "", morseCode[250], morseCodeString;
-	int lenOfDot, lenOfDash, lenOfWordsPause, lenOfLetterPause, lenOfSmallPause, i = 0;
-
 
 	if (!file.is_open()) {
 		cout << "Error! File not found." << endl;
@@ -134,119 +113,12 @@ int main()
 
 	if (checkSamplesNumber(path))
 	{
-
-		int temp = lenOfFirstChar(path); //Duzina prvog znaka
-		if (temp < 22 && temp > 5)
-		{
-			lenOfDot = temp;
-			cout << "Length of dot(.): " << lenOfDot <<"\n"; // .
-
-			lenOfDash = 3 * lenOfDot;
-			cout << "Length of dash(-): " << lenOfDash << "\n"; // -
-
-			lenOfWordsPause = 7 * lenOfDot;
-			cout << "Length of words pause( / ): " << lenOfWordsPause << "\n"; // pause between words( / ) 3xSpace
-
-			lenOfLetterPause = lenOfDash;
-			cout << "Length of letter pause( ): " << lenOfLetterPause << "\n"; // pause between letter( )
-
-			lenOfSmallPause = lenOfDot;
-			cout << "Length of small pause( ): " << lenOfSmallPause << "\n"; // small pause()
-
-		}
-		else if (temp % 3 == 0)
-		{
-			lenOfDash = temp;
-			cout << "Length of dash(-):" << lenOfDash<<"\n"; // -
-
-			lenOfDot = lenOfDash / 3;
-			cout << "Length of dot(.): " << lenOfDot << "\n"; // .
-
-			lenOfWordsPause = 7 * lenOfDot;
-			cout << "Length of words pause( / ): " << lenOfWordsPause << "\n"; // pause between words( / ) 3xSpace
-
-			lenOfLetterPause = lenOfDash;
-			cout << "Length of letter pause( ): " << lenOfLetterPause << "\n"; // pause between letter( )
-
-			lenOfSmallPause = lenOfDot;
-			cout << "Length of small pause( ): " << lenOfSmallPause << "\n"; // small pause()
-		}
-
-		//Signal to Morse------------------------------------------------------------
-		int counter = 0;
-		while (getline(file, line))
-		{
-			++counter;
-			double number = atof(line.c_str());
-			//cout << number << "\n";
-
-			if (number >= 0.520 && number <= 1.600)
-			{
-				if (counter == lenOfDot || counter == lenOfDash)
-				{
-					getline(file, line);
-					number = atof(line.c_str());
-					++counter;
-					if (number >= 0.520 && number <= 1.600 || counter - 1 == lenOfDash)
-					{
-						if (counter - 1 == lenOfDash)
-						{
-							morseCode[i] = "-";
-							morseCodeString += "-";
-							++i;
-							counter = 1;
-						}
-					}
-					else
-					{
-						morseCode[i] = ".";
-						morseCodeString += ".";
-						++i;
-						counter = 1;
-					}
-				}
-			}
-
-			else if (number >= -0.600 && number <= 0.580)
-			{
-				if (counter == lenOfLetterPause || counter == lenOfWordsPause || counter == lenOfSmallPause)
-				{
-					getline(file, line);
-					number = atof(line.c_str());
-					++counter;
-					if (number >= -0.600 && number <= 0.580 || counter - 1 == lenOfWordsPause)
-					{
-						if (counter - 1 == lenOfWordsPause)
-						{
-							morseCode[i] = " / ";
-							morseCodeString += " / ";
-							++i;
-							counter = 1;
-						}
-					}
-					else if (counter - 1 == lenOfLetterPause)
-					{
-						morseCode[i] = " ";
-						morseCodeString += " ";
-						++i;
-						counter = 1;
-					}
-					else if (counter - 1 == lenOfSmallPause)
-					{
-						morseCode[i] = "";
-						morseCodeString += "";
-						++i;
-						counter = 1;
-					}
-				}
-			}
-		}
-		//---------------------------------------------------------------------------
+		converter.setSignalParameters(path);
+		converter.signalToMorse(path);
 	}
 	else {
 		exit(EXIT_FAILURE);
 	}
 
-	print(morseCodeString, morseCode, i);
-
+	print(converter);
 }
